203409470ead25373773b61a79b22beaf49b6062
Chriss Philip Saji <chrissattasseril16@gmail.com>
2026-03-01 00:28:02 +0530

    Delete docker-mono/code directory

--- docker-mono/code/readme.md
+++
@@ -1,1 +0,0 @@
-docker run -d -p 8000:8080 --name cont image
--- docker-mono/code/start.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-service mysql start
-
-sleep 5
-# Run initialization script
-mysq1 -u root < /init.sql
-mvn clean package -DskipTests
-
-java -jar target/chriss_proj-0.0.1-SNAPSHOT. jar
