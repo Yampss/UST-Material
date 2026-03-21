f73f63353ff05c04c3a22a31a849a859673926d8
Yampss <chriss.16hacker@gmail.com>
2026-03-21 10:35:47 +0530

    hipster

--- Online-Boutique-Proj/dockerpush.sh
+++ Online-Boutique-Proj/dockerpush.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-docker build -t cazzzzz/adservice:latest ./src/adservice
-docker push cazzzzz/adservice:latest
-docker build -t cazzzzz/cartservice:latest ./src/cartservice
-docker push cazzzzz/cartservice:latest
-docker build -t cazzzzz/checkoutservice:latest ./src/checkoutservice
-docker push cazzzzz/checkoutservice:latest
-docker build -t cazzzzz/currencyservice:latest ./src/currencyservice
-docker push cazzzzz/currencyservice:latest
-docker build -t cazzzzz/emailservice:latest ./src/emailservice
-docker push cazzzzz/emailservice:latest
-docker build -t cazzzzz/frontend:latest ./src/frontend
-docker push cazzzzz/frontend:latest
-docker build -t cazzzzz/loadgenerator:latest ./src/loadgenerator
-docker push cazzzzz/loadgenerator:latest
-docker build -t cazzzzz/paymentservice:latest ./src/paymentservice
-docker push cazzzzz/paymentservice:latest
-docker build -t cazzzzz/productcatalogservice:latest ./src/productcatalogservice
-docker push cazzzzz/productcatalogservice:latest
-docker build -t cazzzzz/recommendationservice:latest ./src/recommendationservice
-docker push cazzzzz/recommendationservice:latest
-docker build -t cazzzzz/shippingservice:latest ./src/shippingservice
-docker push cazzzzz/shippingservice:latest
+docker build -t cazzzzz/adservice-hipster:latest ./src/adservice
+docker push cazzzzz/adservice-hipster:latest
+docker build -t cazzzzz/cartservice-hipster:latest ./src/cartservice
+docker push cazzzzz/cartservice-hipster:latest
+docker build -t cazzzzz/checkoutservice-hipster:latest ./src/checkoutservice
+docker push cazzzzz/checkoutservice-hipster:latest
+docker build -t cazzzzz/currencyservice-hipster:latest ./src/currencyservice
+docker push cazzzzz/currencyservice-hipster:latest
+docker build -t cazzzzz/emailservice-hipster:latest ./src/emailservice
+docker push cazzzzz/emailservice-hipster:latest
+docker build -t cazzzzz/frontend-hipster:latest ./src/frontend
+docker push cazzzzz/frontend-hipster:latest
+docker build -t cazzzzz/loadgenerator-hipster:latest ./src/loadgenerator
+docker push cazzzzz/loadgenerator-hipster:latest
+docker build -t cazzzzz/paymentservice-hipster:latest ./src/paymentservice
+docker push cazzzzz/paymentservice-hipster:latest
+docker build -t cazzzzz/productcatalogservice-hipster:latest ./src/productcatalogservice
+docker push cazzzzz/productcatalogservice-hipster:latest
+docker build -t cazzzzz/recommendationservice-hipster:latest ./src/recommendationservice
+docker push cazzzzz/recommendationservice-hipster:latest
+docker build -t cazzzzz/shippingservice-hipster:latest ./src/shippingservice
+docker push cazzzzz/shippingservice-hipster:latest
